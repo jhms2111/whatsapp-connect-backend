4b90a54cb8984cf33776eaad62c097a084f6b8b0
Joao Henrique de Morais Serradourada <jhms211190@gmail.com>
2025-11-23 23:34:11 +0100

    COLOCANDO EM PRODUCAO 1

--- node/src/infraestructure/mongo/models/mongoose.ts
+++ node/src/infraestructure/mongo/models/mongoose.ts
@@ -1,6 +1,30 @@
 import mongoose from 'mongoose';
 
 export async function connectToMongoDB() {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    console.error("❌ ERRO: MONGODB_URI não foi definido nas variáveis da Render!");
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    console.log("✅ Conectado ao MongoDB Atlas com sucesso!");
+  } catch (err) {
+    console.error("❌ ERRO AO CONECTAR AO MONGODB ATLAS:", err);
+    process.exit(1);
+  }
+}
+
+
+/*
+import mongoose from 'mongoose';
+
+export async function connectToMongoDB() {
   try {
     await mongoose.connect('mongodb://localhost:27017/ChatSocket');
     console.log('✅ Conectado ao MongoDB via Mongoose');
@@ -8,3 +32,4 @@ export async function connectToMongoDB() {
     console.error('❌ Erro na conexão com MongoDB:', error);
   }
 }
+*/
