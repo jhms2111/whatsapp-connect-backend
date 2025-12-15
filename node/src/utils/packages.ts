0217c75954225e34ba77ada844dd588552bdbf59
Joao Henrique de Morais Serradourada <jhms211190@gmail.com>
2025-12-15 09:23:29 +0100

    parte final precos R$

--- node/src/utils/packages.ts
+++ node/src/utils/packages.ts
@@ -11,16 +11,16 @@ export type BasePkg = {
 
 export const PACKAGES = {
   whatsapp: {
-    29: {
-      priceEuros: 0.50,
-      conversations: 200,
-      priceId: 'price_1SYe4THTEWXeEltkiANpOD6h',
+    589: {
+      priceEuros: 589,
+      conversations: 1250,
+      priceId: 'price_1SeWoeHTEWXeEltkYBJNd484',
       mode: 'subscription',
     },
-    59: {
-      priceEuros: 0.55,
+    349: {
+      priceEuros: 349,
       conversations: 500,
-      priceId: 'price_1SZlPlHTEWXeEltk17NAmKGs',
+      priceId: 'price_1SeWpIHTEWXeEltktFbqDr6S',
       mode: 'subscription',
     },
     99: {
@@ -32,27 +32,27 @@ export const PACKAGES = {
   } as const,
 
   webchat: {
-    // Plano 9 — ainda usando price antigo
-    9: {
-      priceEuros: 0.50,
-      conversations: 100,
-      priceId: 'price_1SYe4THTEWXeEltkiANpOD6h',
+    // Plano 49 — ainda usando price antigo
+    49: {
+      priceEuros: 49,
+      conversations: 200,
+      priceId: 'price_1SeWiZHTEWXeEltkVYFVqYkU',
       mode: 'subscription',
     },
 
-    // Plano 39 — TESTE com price de €0,55
-    39: {
-      priceEuros: 0.55,
-      conversations: 300,
-      priceId: 'price_1SZlPlHTEWXeEltk17NAmKGs', // 👈 seu priceId de 0,55
+    // Plano 99 — TESTE com price de €0,55
+    99: {
+      priceEuros: 99,
+      conversations: 600,
+      priceId: 'price_1SeWlqHTEWXeEltkbZRadfKY', // 👈 seu priceId de 0,55
       mode: 'subscription',
     },
 
-    // Plano 79 — ainda usando price antigo
-    79: {
-      priceEuros: 0.50,
-      conversations: 900,
-      priceId: 'price_1SYe4THTEWXeEltkiANpOD6h',
+    // Plano 139 — ainda usando price antigo
+    139: {
+      priceEuros: 139,
+      conversations: 1000,
+      priceId: 'price_1SeWmVHTEWXeEltkZP7ZY9So',
       mode: 'subscription',
     },
   } as const,
